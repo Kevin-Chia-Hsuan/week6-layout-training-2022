820722cbe36b5aeee6eab992f40a7981fb2d61a3
Kevin Chang <apog780212@gmail.com>
2022-08-29 09:55:03 +0800

    Update 2022-08-29T01:54:58.403Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,21 +1,22 @@
+"use strict";
+
 // Swiper
-const swiper = new Swiper('.swiper', {
+var swiper = new Swiper('.swiper', {
   // Optional parameters
   effect: 'fade',
   fadeEffect: {
-    crossFade: true,
+    crossFade: true
   },
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: '.swiper-pagination'
   },
-
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
+    prevEl: '.swiper-button-prev'
+  }
+}); // AOS
 
-// AOS
 AOS.init();
+//# sourceMappingURL=all.js.map
