b526c8dda850e118ad57e1f1eee88cc3f5f91457
Kevin Chang <apog780212@gmail.com>
2022-08-29 09:54:08 +0800

    <第六週作品初版>

---
+++ app/assets/js/all.js
@@ -0,0 +1,21 @@
+// Swiper
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true,
+  },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+// AOS
+AOS.init();
